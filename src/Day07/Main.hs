ae14e7947bd2d7e2d43dc0979dcff8082e6661e9
John Haynes <john.andrew.haynes@gmail.com>
2025-12-07 17:11:08 +0100

    start day7

---
+++ src/Day07/Main.hs
@@ -0,0 +1,15 @@
+module Main ( main ) where
+
+import Combinators
+import Parser
+import String
+
+import Control.Applicative
+import Control.Monad
+
+main :: IO ()
+main = do
+
+    input <- readFile "./sample_7"
+
+    pure ()
